ee5519766031afece91b5ce9e5bcfb8c972d2f4d
Connor Elsea <connorelsea@gmail.com>
2015-05-26 10:37:18 -0500

    Added IP/port for OpenShift, fixed cross-browser support

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var express = require("express"),
 
 var app = express()
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 function compile(str, path)
 {
   return stylus(str)
@@ -50,7 +53,7 @@ app.post('/contact', function(req, res) {
 	res.redirect("/")
 })
 
-app.listen(1337, function()
+app.listen(server_port, server_ip_address, function()
 {
 	console.log("Ready on the port")
 })
